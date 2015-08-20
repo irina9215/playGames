55d9856e2002203d7f2f8bea04e759dfe4655377
莫颜鲜 <moyanxian@breadtrip.com>
2015-08-20 10:30:54 +0800

    用白鹭引擎搭游戏之认识篇

---
+++ egret/demo1/HelloWorld/bin-debug/src/Main.js
@@ -0,0 +1,193 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2014-2015, Egret Technology Inc.
+//  All rights reserved.
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Egret nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
+//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////////////
+var Main = (function (_super) {
+    __extends(Main, _super);
+    function Main() {
+        _super.call(this);
+        this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
+    }
+    var __egretProto__ = Main.prototype;
+    __egretProto__.onAddToStage = function (event) {
+        //设置加载进度界面
+        //Config to load process interface
+        this.loadingView = new LoadingUI();
+        this.stage.addChild(this.loadingView);
+        //初始化Resource资源加载库
+        //initiate Resource loading library
+        RES.addEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
+        RES.loadConfig("resource/resource.json", "resource/");
+    };
+    /**
+     * 配置文件加载完成,开始预加载preload资源组。
+     * configuration file loading is completed, start to pre-load the preload resource group
+     */
+    __egretProto__.onConfigComplete = function (event) {
+        RES.removeEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
+        RES.loadGroup("lang");
+    };
+    /**
+     * preload资源组加载完成
+     * Preload resource group is loaded
+     */
+    __egretProto__.onResourceLoadComplete = function (event) {
+        if (event.groupName == "lang") {
+            this.stage.removeChild(this.loadingView);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
+            this.createGameScene();
+        }
+    };
+    /**
+     * 资源组加载出错
+     *  The resource group loading failed
+     */
+    __egretProto__.onResourceLoadError = function (event) {
+        //TODO
+        console.warn("Group:" + event.groupName + " has failed to load");
+        //忽略加载失败的项目
+        //Ignore the loading failed projects
+        this.onResourceLoadComplete(event);
+    };
+    /**
+     * preload资源组加载进度
+     * Loading process of preload resource group
+     */
+    __egretProto__.onResourceProgress = function (event) {
+        if (event.groupName == "preload") {
+            this.loadingView.setProgress(event.itemsLoaded, event.itemsTotal);
+        }
+    };
+    /**
+     * 创建游戏场景
+     * Create a game scene
+     */
+    __egretProto__.createGameScene = function () {
+        var bg = new egret.Shape;
+        bg.graphics.beginFill(0x336699);
+        bg.graphics.drawRect(0, 0, this.stage.stageWidth, this.stage.stageHeight);
+        bg.graphics.endFill();
+        _super.prototype.addChild.call(this, bg);
+        var tx = new egret.TextField;
+        tx.text = "let's play a hide&find game!'";
+        tx.size = 32;
+        _super.prototype.addChild.call(this, tx);
+        tx.x = 20;
+        tx.y = 20;
+        tx.width = this.stage.stageWidth - 40;
+        tx.touchEnabled = true;
+        tx.addEventListener(egret.TouchEvent.TOUCH_TAP, this.touchhandler, this);
+        var github = new egret.Bitmap(RES.getRes("github"));
+        github.x = 150;
+        github.y = 200;
+        _super.prototype.addChild.call(this, github);
+        var go = new egret.Bitmap(RES.getRes("go"));
+        go.x = -10;
+        go.y = 100;
+        _super.prototype.addChild.call(this, go);
+        go.x = go.x + go.width / 2;
+        go.y = go.y + go.height;
+        _super.prototype.setChildIndex.call(this, github, _super.prototype.getChildIndex.call(this, go));
+        this.times = -1;
+        var self = this;
+        this.stage.addEventListener(egret.TouchEvent.TOUCH_TAP, function () {
+            switch (++self.times % 2) {
+                case 0:
+                    {
+                        egret.Tween.get(go).to({ x: github.x }, 300, egret.Ease.circIn);
+                        egret.Tween.get(github).to({ x: go.x }, 300, egret.Ease.circIn);
+                    }
+                    ;
+                case 1:
+                    {
+                        egret.Tween.get(go).to({ scalaX: 0.4, scaleY: 1 }, 500, egret.Ease.circIn).to({ scaleX: 1, scaleY: 1 }, 500, egret.Ease.circIn);
+                    }
+                    ;
+            }
+        }, this);
+    };
+    /**
+     * touchhandler
+     * 触摸句柄
+     */
+    __egretProto__.touchhandler = function (evt) {
+        var tx = evt.currentTarget;
+        tx.textColor = 0x00ff00;
+    };
+    /**
+     * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
+     * Create a Bitmap object according to name keyword.As for the property of name please refer to the configuration file of resources/resource.json.
+     */
+    __egretProto__.createBitmapByName = function (name) {
+        var result = new egret.Bitmap();
+        var texture = RES.getRes(name);
+        result.texture = texture;
+        return result;
+    };
+    /**
+     * 描述文件加载成功，开始播放动画
+     * Description file loading is successful, start to play the animation
+     */
+    __egretProto__.startAnimation = function (result) {
+        var self = this;
+        var parser = new egret.HtmlTextParser();
+        var textflowArr = [];
+        for (var i = 0; i < result.length; i++) {
+            textflowArr.push(parser.parser(result[i]));
+        }
+        var textfield = self.textfield;
+        var count = -1;
+        var change = function () {
+            count++;
+            if (count >= textflowArr.length) {
+                count = 0;
+            }
+            var lineArr = textflowArr[count];
+            self.changeDescription(textfield, lineArr);
+            var tw = egret.Tween.get(textfield);
+            tw.to({ "alpha": 1 }, 200);
+            tw.wait(2000);
+            tw.to({ "alpha": 0 }, 200);
+            tw.call(change, self);
+        };
+        change();
+    };
+    /**
+     * 切换描述内容
+     * Switch to described content
+     */
+    __egretProto__.changeDescription = function (textfield, textFlow) {
+        textfield.textFlow = textFlow;
+    };
+    return Main;
+})(egret.DisplayObjectContainer);
+Main.prototype.__class__ = "Main";
